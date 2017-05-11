d3bd3dff471e8845466e3b46acb587b4905430d1
Ulf <ulfhjohlman@gmail.com>
2017-05-12 01:36:32 +0200

    rewrite of output

--- lab3/main.c
+++ lab3/main.c
@@ -15,7 +15,7 @@ void scan(FILE * inputFile,double * coord, int *r){
     return;
 }
 
-void compute(double * coord, int * count,int * low_count,int k,int t){
+void compute(double * coord, int * count,int * low_count, int * real_count,int k,int t){
     double x,y,z;
     double dist;
     int i,j;
@@ -23,6 +23,7 @@ void compute(double * coord, int * count,int * low_count,int k,int t){
     {
         int * count_private= calloc(120063, sizeof(int));
 	    int * low_count_private = calloc(100,sizeof(int));
+        int * real_count_private= calloc(3465,sizeof(int));
         #pragma omp for schedule(dynamic) private(i,j,x,y,z,dist)
         for(i=0;i<k;i+=3){
             #pragma omp simd
@@ -32,15 +33,16 @@ void compute(double * coord, int * count,int * low_count,int k,int t){
                 z = coord[i+2] - coord[j+2];
                 //distance^2 between 2 points rounded to hundredths, 12.232 -> 1223
                 //dist = roundf((x*x+y*y+z*z)*100);
-                dist = (x*x+y*y+z*z)*100;
+                dist = (x*x+y*y+z*z);
 		//printf("dist: %.2lf\n", (float)((int)roundf(dist*100))/100);
 		//#pragma omp atomic
                 //count[(int) dist]++;
-		if(dist<100){
-			low_count_private[ (int)(sqrt(dist/100)*100)]++;
+                real_count_private[(int)(0.5+sqrt(dist)*100)]++;
+        if(dist<1){
+			low_count_private[ (int)(0.5+sqrt(dist)*100)]++;
 		}
         else{
-		      count_private[(int) dist]++;
+		      count_private[(int) (dist*100)]++;
         }
             }
         }
@@ -49,8 +51,11 @@ void compute(double * coord, int * count,int * low_count,int k,int t){
 		for(int l=0;l<120063;l++){
 			count[l]+= count_private[l];
 		}
-		for(int l=0;l<100;l++){
+        for(int l=0;l<100;l++){
 			low_count[l]+=low_count_private[l];
+		}
+        for(int l=0;l<3465;l++){
+			real_count[l]+=real_count_private[l];
 		}
 	}
     }
@@ -103,44 +108,36 @@ int main(int argc, char *argv[]){
     coord = malloc(100000*3*sizeof(double));
     //float * coord; //coordinates of all points
     //coord = malloc(10e5*3*sizeof(float));
-
-    int * count; // holds count of distances between points in increments of 0.01
-    int * low_count;
-    int MAX_COUNT = 120063;// round_up(20 * sqrt(3) * 100)^2 ints (the max distance^2 possible is 34.65^2);
-    count = calloc(MAX_COUNT,sizeof(int));
-    low_count = calloc(100,sizeof(int));
     int k;
     scan(inputFile,coord,&k);
 
+    int * count; // holds count of distances between points in increments of 0.01
+    int * low_count;
+    int * real_count;
+    int MAX_COUNT = 120063;// round_up(20 * sqrt(3) * 100)^2 ints (the max distance^2 possible is 34.65^2);
+    count = calloc(MAX_COUNT,sizeof(int));
+    low_count = calloc(100,sizeof(int));
+    real_count = calloc(3465,sizeof(int));
+
+    compute(coord,count,low_count,real_count,k,t);
 
     int i,j;
-    compute(coord,count,low_count,k,t);
-
-
     for(i=0;i<100;i++){
         if(low_count[i]>0){
-            printf("%.2f %d\n", i*0.01 , low_count[i]);
+            printf("%.2f %d   real: %d\n", i*0.01 , low_count[i],real_count[i]);
         }
     }
     int c=0;
-    j=101;
-    double sqrtBin = (int)( (j / 100.0)*(j / 100.0)*100.0);
+    j=100;
+    double sqrtBin;
     for(i=100;i<MAX_COUNT;i++){
-        if(count[i]>0){
-        //printf("uBin: %.2lf , i: %d, j: %d, count[i]: %d\n",sqrtBin,i,j,count[i]);
-        }
-        if( i < sqrtBin){
-            c+=count[i];
-        }else
-        {
-            if(c>0){
-                printf("%.2f %d\n",(j-1)*0.01 , c);
-                c=0;
-            }
+        sqrtBin = sqrt(i/100.0)*100.0;
+        if(sqrtBin > j + 0.5){
+            printf("%.2f %d    real: %d\n",j*0.01 , c, real_count[j]);
+            c=0;
             j++;
-            sqrtBin = (int)( (j / 100.0)*(j / 100.0)*100.0);
-            c+=count[i];
         }
+        c+=count[i];
     }
     fclose(inputFile);
     free(coord);
