21837d79bb39212d35742b8c27ca65f1b3aa91ce
Ulf <ulfhjohlman@gmail.com>
2017-05-12 11:32:20 +0200

    revert to basics

--- lab3/main.c
+++ lab3/main.c
@@ -5,63 +5,6 @@
 #include<math.h>
 #include<time.h>
 
-void scan(FILE * inputFile,double * coord, int *r){
-    int k=0;
-    while(fscanf(inputFile,"%lf %lf %lf\n",&coord[k],&coord[k+1],&coord[k+2]) == 3){
-    //while(fscanf(inputFile,"%f %f %f\n",&coord[k],&coord[k+1],&coord[k+2]) == 3){
-        k+=3;
-    }
-    *r = k;
-    return;
-}
-
-void compute(double * coord, int * count,int * low_count, int * real_count,int k,int t){
-    double x,y,z;
-    double dist;
-    int i,j;
-    #pragma omp parallel num_threads(t)
-    {
-        int * count_private= calloc(120063, sizeof(int));
-	    int * low_count_private = calloc(100,sizeof(int));
-        int * real_count_private= calloc(3465,sizeof(int));
-        #pragma omp for schedule(dynamic) private(i,j,x,y,z,dist)
-        for(i=0;i<k;i+=3){
-            #pragma omp simd
-	    for(j=i+3;j<k;j+=3){
-                x = coord[i] - coord[j];
-                y = coord[i+1] - coord[j+1];
-                z = coord[i+2] - coord[j+2];
-                //distance^2 between 2 points rounded to hundredths, 12.232 -> 1223
-                //dist = roundf((x*x+y*y+z*z)*100);
-                dist = (x*x+y*y+z*z);
-		//printf("dist: %.2lf\n", (float)((int)roundf(dist*100))/100);
-		//#pragma omp atomic
-                //count[(int) dist]++;
-                real_count_private[(int)(0.5+sqrt(dist)*100)]++;
-        if(dist<1){
-			low_count_private[ (int)(0.5+sqrt(dist)*100)]++;
-		}
-        else{
-		      count_private[(int) (dist*100)]++;
-        }
-            }
-        }
-	#pragma omp critical
-	{
-		for(int l=0;l<120063;l++){
-			count[l]+= count_private[l];
-		}
-        for(int l=0;l<100;l++){
-			low_count[l]+=low_count_private[l];
-		}
-        for(int l=0;l<3465;l++){
-			real_count[l]+=real_count_private[l];
-		}
-	}
-    }
-    return;
-}
-
 int main(int argc, char *argv[]){
     /* parse command line arguments */
     unsigned int t=2;
@@ -108,36 +51,49 @@ int main(int argc, char *argv[]){
     coord = malloc(100000*3*sizeof(double));
     //float * coord; //coordinates of all points
     //coord = malloc(10e5*3*sizeof(float));
-    int k;
-    scan(inputFile,coord,&k);
+    int k=0;
+    while(fscanf(inputFile,"%lf %lf %lf\n",&coord[k],&coord[k+1],&coord[k+2]) == 3){
+    //while(fscanf(inputFile,"%f %f %f\n",&coord[k],&coord[k+1],&coord[k+2]) == 3){
+        k+=3;
+    }
 
     int * count; // holds count of distances between points in increments of 0.01
-    int * low_count;
-    int * real_count;
-    int MAX_COUNT = 120063;// round_up(20 * sqrt(3) * 100)^2 ints (the max distance^2 possible is 34.65^2);
+    int MAX_COUNT = 3465;// round_up(20 * sqrt(3) * 100)^2 ints (the max distance^2 possible is 34.65^2);
     count = calloc(MAX_COUNT,sizeof(int));
-    low_count = calloc(100,sizeof(int));
-    real_count = calloc(3465,sizeof(int));
 
-    compute(coord,count,low_count,real_count,k,t);
-
+    double x,y,z;
+    double dist;
     int i,j;
-    for(i=0;i<100;i++){
-        if(low_count[i]>0){
-            printf("%.2f %d   real: %d\n", i*0.01 , low_count[i],real_count[i]);
+    #pragma omp parallel num_threads(t)
+    {
+        int * count_private= calloc(12006300, sizeof(int));
+	    #pragma omp for schedule(dynamic) private(i,j,x,y,z,dist)
+        for(i=0;i<k;i+=3){
+            #pragma omp simd
+	        for(j=i+3;j<k;j+=3){
+                x = coord[i] - coord[j];
+                y = coord[i+1] - coord[j+1];
+                z = coord[i+2] - coord[j+2];
+                //distance between 2 points rounded to hundredths, 12.232 -> 1223
+                //dist = roundf((x*x+y*y+z*z)*100);
+                dist = sqrt(x*x+y*y+z*z);
+		        //printf("dist: %.2lf\n", (float)((int)roundf(dist*100))/100);
+              count_private[(int) (dist*100)]++;
+          }
         }
+    	#pragma omp critical
+    	{
+    		for(int l=0;l<3465;l++){
+    			count[l]+= count_private[l];
+    		}
+    	}
+        free(count_private);
     }
-    int c=0;
-    j=100;
-    double sqrtBin;
-    for(i=100;i<MAX_COUNT;i++){
-        sqrtBin = sqrt(i/100.0)*100.0;
-        if(sqrtBin > j + 0.5){
-            printf("%.2f %d    real: %d\n",j*0.01 , c, real_count[j]);
-            c=0;
-            j++;
+
+    for(i=1;i<MAX_COUNT;i++){
+        if(count[i] > 0){
+            printf("%.2f %d\n",i*0.01,count[i]);
         }
-        c+=count[i];
     }
     fclose(inputFile);
     free(coord);
