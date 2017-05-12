fbf2d20f781d02143e550318bc33750fc993649a
Ulf H <ulfhjohlman@gmail.com>
2017-05-12 12:03:45 +0200

    report, tar, and final touches

--- lab3/main.c
+++ lab3/main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]){
     }
 
 
-    //make cells file
+    //if input option n is specified make cells file
     if(n!=0){
         FILE * f =fopen("cells","w");
         if(f == NULL){
@@ -49,16 +49,13 @@ int main(int argc, char *argv[]){
     }
     double * coord; //coordinates of all points
     coord = malloc(100000*3*sizeof(double));
-    //float * coord; //coordinates of all points
-    //coord = malloc(10e5*3*sizeof(float));
     int k=0;
     while(fscanf(inputFile,"%lf %lf %lf\n",&coord[k],&coord[k+1],&coord[k+2]) == 3){
-    //while(fscanf(inputFile,"%f %f %f\n",&coord[k],&coord[k+1],&coord[k+2]) == 3){
         k+=3;
     }
 
     int * count; // holds count of distances between points in increments of 0.01
-    int MAX_COUNT = 3465;// round_up(20 * sqrt(3) * 100)^2 ints (the max distance^2 possible is 34.65^2);
+    int MAX_COUNT = 3465;// round_up(20 * sqrt(3) * 100) ints (the max distance possible is 34.65)
     count = calloc(MAX_COUNT,sizeof(int));
 
     double x,y,z;
@@ -66,19 +63,17 @@ int main(int argc, char *argv[]){
     int i,j;
     #pragma omp parallel num_threads(t)
     {
-        int * count_private= calloc(12006300, sizeof(int));
+        int * count_private= calloc(3465, sizeof(int));
 	    #pragma omp for schedule(dynamic) private(i,j,x,y,z,dist)
         for(i=0;i<k;i+=3){
-            #pragma omp simd
 	        for(j=i+3;j<k;j+=3){
                 x = coord[i] - coord[j];
                 y = coord[i+1] - coord[j+1];
                 z = coord[i+2] - coord[j+2];
-                //distance between 2 points rounded to hundredths, 12.232 -> 1223
-                //dist = roundf((x*x+y*y+z*z)*100);
+                //distance between 2 points
                 dist = sqrt(x*x+y*y+z*z);
-		        //printf("dist: %.2lf\n", (float)((int)roundf(dist*100))/100);
-              count_private[(int) (dist*100)]++;
+		//round to nearest 0.01 and increment that count
+              count_private[(int) (0.5+dist*100)]++;
           }
         }
     	#pragma omp critical
--- lab3/report.markdown
+++ lab3/report.markdown
@@ -1,3 +1,4 @@
 #Assignment 2
 ##Ulf Hjohlman
 ###Program Description
+The program is based around efficiently calculating the distances between points in parallel and increasing a shared counter for each occurence of a distance (what points correlate to each distance is not stored). Parallelization is done with openmp and the shared `count` is split into private objects for each thread and then calculated through reduction at the end in an `#pragma omp critical`. Scheduling is done through `schedule(dynamic)` because early loop iterations will take longer than the later ones. The output is then printed as specified.
