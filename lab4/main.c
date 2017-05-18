6cfc2f8f68fc74f43bc13b8ab1688472a8b48d40
Ulf H <ulfhjohlman@gmail.com>
2017-05-18 19:55:53 +0200

    working!

--- lab4/main.c
+++ lab4/main.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[]){
     /* parse command line arguments */
     int w=3;
     int h=3;
-    double i=1e6;
+    float i=1e6;
     int n=2;
-    double d=1/30;
+    float d=(1.0/30);
     int opt;
     while ((opt = getopt(argc, argv, "i:n:d:")) != -1) {
         switch (opt) {
-            case 'i': i = (double) strtod(optarg,(char**)NULL); break;
+            case 'i': i = (float) strtof(optarg,(char**)NULL); break;
             case 'n': n = (int) strtol(optarg,(char**)NULL,10); break;
-            case 'd': d = (double) strtod(optarg,(char**)NULL); break;
+            case 'd': d = (float) strtof(optarg,(char**)NULL); break;
             default:
                 fprintf(stderr, "Usage: %s [width] [height] [-i][centerValue] [-d][diffusionConstant] [-n][nIterations]\n", argv[0]);
                 exit(EXIT_FAILURE);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]){
         w = strtol(argv[optind],(char**)NULL,10);
         h = strtol(argv[optind+1],(char**)NULL,10);
     }
-    printf("w: %d, h: %d i:%fl n: %d, d :%lf\n",w,h,i,n,d);
+    printf("w: %d, h: %d i:%f n: %d, d :%f\n",w,h,i,n,d);
     
     cl_int error;
     cl_platform_id platform_id;
@@ -57,28 +57,28 @@ int main(int argc, char *argv[]){
     
     const char * opencl_program_src = 
 	"__kernel void diffuse(\
-	__global const int * h,\
-	__global const int * w,\
-	__global const int * c,\
-	__global float * old,\
+	int h,\
+	int w,\
+	float c,\
+	__global const float * old,\
 	__global float * new){\
 	int ix = get_global_id(0);\
 	float t,d,l,r;\
-	if(ix<*w){\
+	if(ix<w){\
 		t=0;\
-	}else{ t = old[ix-*w];}\
-	if(ix>= (*h-1)* *w){\
+	}else{ t = old[ix-w];}\
+	if(ix>= (h-1)* w){\
 		d=0;\
-	}else{ d = old[ix+ *w];}\
-	if(ix % *w ==0){\
+	}else{ d = old[ix+ w];}\
+	if(ix % w ==0){\
 		l=0;\
 	}else{ l = old[ix-1];}\
-	if( (ix-1)% *w ==0){\
+	if( (ix-1)% w ==0){\
 		r = 0;\
 	}else{ r = old[ix+1];}\
-	new[ix] = old[ix] + *c*((t+d+l+r)*0.25 -old[ix]);\
+	new[ix] = old[ix]+ c*((t+d+l+r)*0.25 - old[ix]);\
 	}";
-printf("1\n");
+
     cl_program program;
     program = clCreateProgramWithSource(context,1,(const char **) &opencl_program_src, NULL, &error);
     if(error != CL_SUCCESS) {
@@ -102,7 +102,6 @@ printf("1\n");
 	free(log);
 	return 1;
     }
-printf("2\n");
     cl_kernel kernel;
     kernel = clCreateKernel(program, "diffuse", &error);
     if(error != CL_SUCCESS) {
@@ -122,10 +121,10 @@ printf("2\n");
         printf("cannot create buffer b\n");
         return 1;
     }
-    input_buffer_h  = clCreateBuffer(context, CL_MEM_READ_ONLY,sizeof(int) * size, NULL, &error);
-    input_buffer_w  = clCreateBuffer(context, CL_MEM_READ_ONLY,sizeof(int) * size, NULL, &error);
-    input_buffer_d  = clCreateBuffer(context, CL_MEM_READ_ONLY,sizeof(int) * size, NULL, &error);
-printf("3\n");    
+//    input_buffer_h  = clCreateBuffer(context, CL_MEM_READ_ONLY,sizeof(int) * size, NULL, &error);
+//    input_buffer_w  = clCreateBuffer(context, CL_MEM_READ_ONLY,sizeof(int) * size, NULL, &error);
+//    input_buffer_d  = clCreateBuffer(context, CL_MEM_READ_ONLY,sizeof(int) * size, NULL, &error);
+   
     float * a = (float *) calloc(size,sizeof(float));
     float * b = (float *) calloc(size,sizeof(float));
     if(h%2 == 0){
@@ -153,7 +152,9 @@ printf("3\n");
 		} printf("\n");
     }
     
-printf("4\n");
+
+    float * tmp;
+    for(int i=0;i<n;i++){
     error = clEnqueueWriteBuffer(command_queue, input_buffer_a, CL_TRUE,0, size*sizeof(float), a, 0, NULL, NULL);
     if(error != CL_SUCCESS) {
         printf("cannot enqueue buffer a \n");
@@ -164,28 +165,29 @@ printf("4\n");
         printf("cannot enqueue buffer b \n");
         return 1;
     }
-    error = clEnqueueWriteBuffer(command_queue, input_buffer_h, CL_TRUE, 0, size*sizeof(int), &h, 0 ,NULL, NULL);
-    if(error != CL_SUCCESS) {
-	printf("cannot enqueue buffer h\n");
-	return(1);
-    }
-    error = clEnqueueWriteBuffer(command_queue, input_buffer_w, CL_TRUE, 0, size*sizeof(int), &w, 0 ,NULL, NULL);
-    error = clEnqueueWriteBuffer(command_queue, input_buffer_d, CL_TRUE, 0, size*sizeof(int), &d, 0 ,NULL, NULL);
-printf("setting constant args:\n");
-    error = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer_h);
+//    error = clEnqueueWriteBuffer(command_queue, input_buffer_h, CL_TRUE, 0, size*sizeof(int), &h, 0 ,NULL, NULL);
+//    if(error != CL_SUCCESS) {
+//	printf("cannot enqueue buffer h\n");
+//	return(1);
+//    }
+//    error = clEnqueueWriteBuffer(command_queue, input_buffer_w, CL_TRUE, 0, size*sizeof(int), &w, 0 ,NULL, NULL);
+//    error = clEnqueueWriteBuffer(command_queue, input_buffer_d, CL_TRUE, 0, size*sizeof(int), &d, 0 ,NULL, NULL);
+
+//printf("setting constant args:\n");
+    error = clSetKernelArg(kernel, 0, sizeof(int), &h);
     if(error != CL_SUCCESS) {
         printf("cannot set kernel arg h\n");
         return 1;
     }
-    error = clSetKernelArg(kernel, 1, sizeof(cl_mem), &input_buffer_w);
+    error = clSetKernelArg(kernel, 1, sizeof(int), &w);
     if(error != CL_SUCCESS) {
 	printf("cannot set kernel arg w\n");
     }
-    error = clSetKernelArg(kernel, 2, sizeof(cl_mem), &input_buffer_d);
+    error = clSetKernelArg(kernel, 2, sizeof(float), &d);
     if(error != CL_SUCCESS) {
 	printf("cannot set kernel arg d\n");
     }
-printf("setting buffer args:\n");
+//printf("setting buffer args:\n");
     error = clSetKernelArg(kernel, 3, sizeof(cl_mem), &input_buffer_a);
     if(error != CL_SUCCESS) {
 	printf("cannot set kernel arg input_buffer_a");
@@ -194,7 +196,7 @@ printf("setting buffer args:\n");
     if(error != CL_SUCCESS) {
 	printf("cannot set kernel arg input_buffer_b");
     }
-printf("Enquing:\n");
+//printf("Enquing:\n");
     error = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,(const size_t *)&size, NULL, 0, NULL, NULL);
     if(error != CL_SUCCESS) {
         printf("cannot enqueue nd range kernel\n");
@@ -203,8 +205,9 @@ printf("Enquing:\n");
     
     clFinish(command_queue);
 
-printf("reading:\n");
+//printf("reading:\n");
     error = clEnqueueReadBuffer(command_queue, input_buffer_a, CL_TRUE,0, size*sizeof(float), a, 0, NULL, NULL);
+
     if(error != CL_SUCCESS) {
         printf("cannot read buffer a\n");
         return 1;
@@ -214,8 +217,14 @@ printf("reading:\n");
 	printf("cannot read buffer b\n");
 	return 1;
     }
+
+
     clFinish(command_queue);
 
+    tmp = b;
+    b = a;
+    a = tmp;
+    /*
     if(size <= 100){
     printf("Printing a:\n");
     for(int i=0;i<h;i++){
@@ -232,7 +241,10 @@ printf("reading:\n");
 		printf(" %f ",b[i*h+j]);
 	}
 	printf("\n");
-    }}
+        }
+    }*/
+    }
+
     free(a);
     free(b);
     clReleaseMemObject(input_buffer_a);
